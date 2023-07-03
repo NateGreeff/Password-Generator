092575f81d65212f58093f0740acf448aeaa6106
Nathan Greeff <NGreeff@slco.org>
2023-07-02 18:27:44 -0600

    Added password length check

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Setting password requirements
+function generatePassword() {
+  var passwordLength = prompt("How long would you like your password to be? Please enter a number between 8 and 128")
+  while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) { //Logic to check password length requirements
+    if (passwordLength === null) { //Allows user to cancel if they want to
+      return;
+    }
+    passwordLength = prompt(`"${passwordLength}" is not valid. Please enter a number between 8 and 128.`)
+  }
+};
